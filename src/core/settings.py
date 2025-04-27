a0eb928ec6f2b5df5e4d81a7c46e0ebd42ab3246
Anastasiya <aiskoskina@yandex.ru>
2025-04-27 14:51:35 +0300

    add connect to postgres

---
+++ src/core/settings.py
@@ -0,0 +1,23 @@
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Settings(BaseSettings):
+    postgres_db: str
+    db_port: int
+    db_host: str
+    postgres_user: str
+    postgres_password: str
+
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+
+settings = Settings()  # type: ignore
---
+++ src/main.py
@@ -0,0 +1,31 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from .api.v1.routes import router
+from .db.postgres.connection import create_database
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    await create_database()
+    yield
+
+
+app = FastAPI(
+    docs_url="/api/docs",
+    openapi_url="/api/docs.json",
+    lifespan=lifespan,
+)
+
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=8000,
+        reload=True,
+    )

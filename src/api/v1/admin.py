f51c0ec11ffa66b0c14c785fe6dffc3c9a2973e5
Anastasiya <aiskoskina@yandex.ru>
2025-04-27 14:51:11 +0300

    add admin router

---
+++ src/api/v1/admin.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.db.postgres.connection import get_session
+
+router = APIRouter()
+
+
+@router.post(
+    "/auth",
+    status_code=status.HTTP_200_OK,
+    description="Admin authorization",
+)
+async def auth(
+session: AsyncSession = Depends(get_session)
+):
+    # try:
+    #     await signup(user_create.model_dump(), session)
+    # except Exception as exc:
+    #     logger.error(f"msg=Error user registration: {exc}")
+    #     raise HTTPException(
+    #         status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail="user not created"
+    #     ) from exc
+    return "ok"
